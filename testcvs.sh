357c7972a645b1afe9dc2fef5d6834c67218f770
Kamil Monticolo <kmonticolo+github@gmail.com>
2016-02-25 10:46:38 +0100

    cosmetic changes

--- testcvs.sh
+++ testcvs.sh
@@ -7,12 +7,12 @@ ftp -o - http://www.openbsd.org/anoncvs.html >anoncvs.html
 grep CVSROOT= anoncvs.html |sed -e 's/^.*@//g' -e 's/:.*$//g' >serwery 2>/dev/null
 #curl http://www.openbsd.org/anoncvs.html |grep CVSROOT= |sed -e 's/^.*@//g' -e 's/:.*$//g' >serwery
 wait=`cat serwery|wc -l`
-echo -n "wait " `echo "( $wait * 3 )"| bc` "seconds."
-for i in `cat serwery`;do j=`ping -q -c3 $i|grep round| cut -d \/ -f 5|cut -f1 -d .`; echo -n $i " " $j >>pingidoserwerow; echo -n .; echo >>pingidoserwerow; done
+echo -n "Wait" `echo "( $wait * 3 )"| bc` "seconds."
+for i in `cat serwery`;do j=`ping -q -c3 $i 2>/dev/null |grep round| cut -d \/ -f 5|cut -f1 -d .`; echo -n $i " " $j >>pingidoserwerow; echo -n .; echo >>pingidoserwerow; done
 #grep round pingidoserwerow |sed -e s'/\//\;/g' -e 's/\./,/g'
 echo
 cat pingidoserwerow |grep [0-9]$ | sort -nk 2 |sed 's/$/\ ms/g'
-echo The fastest mirror is:
+echo; echo The fastest mirror is:
 grep `cat pingidoserwerow |grep [0-9]$ | sort -nk 2 |head -1 |cut -f 1 -d" "` anoncvs.html |sed -e 's/^.*CVSROOT\>/CVSROOT/' -e 's/:\/cvs.*$/:\/cvs/g'
 
 #EOF
