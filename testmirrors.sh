2112f7ad86a3b2306944fb0e3fe573713ef3ea5f
Kamil Monticolo <kmonticolo+github@gmail.com>
2016-09-07 11:24:20 +0200

    cleanup, export $FASTEST env
    export $FASTEST env

--- testmirrors.sh
+++ testmirrors.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-# 25.02.16 kmonti
+# 25.02.16, 07.09.16 kmonti
 # script for test speed of OpenBSD mirrors
 
->pingidoserwerow
-ftp -o - http://www.openbsd.org/ftp.html >ftp.html
-grep "OpenBSD\/" ftp.html | grep "/</a>"| grep http|sed -e 's/^........//g' -e 's/\/.*$//' >serwery
+>/tmp/pingidoserwerow
+ftp -o - http://www.openbsd.org/ftp.html >/tmp/ftp.html
+grep "OpenBSD\/" /tmp/ftp.html | grep "/</a>"| grep http|sed -e 's/^........//g' -e 's/\/.*$//' >/tmp/serwery
+grep "/</a>" /tmp/ftp.html |sed -e 's/^.*:\/\///g' -e 's/\/.*$//' |sort|uniq > /tmp/serwery
 # zrobic http i ftp
-wait=`cat serwery|wc -l`
+wait=`cat /tmp/serwery | wc -l`
 echo -n "wait" `echo "( $wait * 3 )"| bc` "seconds."
-for i in `cat serwery`;do j=`ping -q -c3 $i 2>/dev/null |grep round| cut -d \/ -f 5|cut -f1 -d .`; echo -n $i " " $j >>pingidoserwerow; echo -n .; echo >>pingidoserwerow; done
-grep round pingidoserwerow |sed -e s'/\//\;/g' -e 's/\./,/g'
+for i in `cat /tmp/serwery`;
+  do j=`ping -q -c3 $i 2>/dev/null |grep round| cut -d \/ -f 5|cut -f1 -d .`
+  echo -n $i " " $j >>/tmp/pingidoserwerow
+  echo -n .
+  echo >>/tmp/pingidoserwerow
+done
+grep round /tmp/pingidoserwerow |sed -e s'/\//\;/g' -e 's/\./,/g'
 echo
-cat pingidoserwerow |grep [0-9]$ | sort -nk 2 |sed 's/$/\ ms/g'
-echo; echo -n "The fastest mirror is: "
-echo `cat pingidoserwerow |grep [0-9]$ | sort -nk 2 |head -1 |cut -f 1 -d" "`
-\rm -f pingidoserwerow serwery
-
+#grep [0-9]$ /tmp/pingidoserwerow | sort -nk 2 |sed 's/$/\ ms/g'
+fastest=`grep [0-9]$ /tmp/pingidoserwerow | sort -nk 2 |head -1 |cut -f 1 -d" "`
+export FASTEST="http://${fastest}/pub/OpenBSD"
+echo; echo -n "The fastest mirror is: $FASTEST"
+echo
+\rm -f /tmp/{pingidoserwerow,serwery,ftp.html}
 #EOF
