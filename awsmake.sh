a1de57c3db4555201e391e4947774191bbd7af1e
Kamil Monticolo <kmonticolo+github@gmail.com>
2017-03-02 13:47:37 +0100

    Update awsmake.sh

--- awsmake.sh
+++ awsmake.sh
@@ -9,7 +9,9 @@
 
 # Ireland
 export MIRROR=https://ftp.heanet.ie/pub/OpenBSD/
+export PKG_PATH=$MIRROR/6.0/packages/amd64
 export TMPDIR=/home
+
 ftp -o create-ami.sh https://raw.githubusercontent.com/ajacoutot/aws-openbsd/master/create-ami.sh                                        
 test -f create-ami.sh && chmod +x create-ami.sh
 ./create-ami.sh -n
