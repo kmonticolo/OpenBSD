01f91ed6882a8a816772a0012986a01262961ba3
Kamil Monticolo <kmonticolo+github@gmail.com>
2017-03-02 13:35:53 +0100

    Update awsmake.sh

--- awsmake.sh
+++ awsmake.sh
@@ -2,7 +2,14 @@
 
 #
 # get mirror list script
-ftp -o testmirrors.sh https://raw.githubusercontent.com/kmonticolo/OpenBSD/master/testmirrors.sh
-test -f testmirrors.sh && chmod +x testmirrors.sh
-./testmirrors.sh
+#ftp -o testmirrors.sh https://raw.githubusercontent.com/kmonticolo/OpenBSD/master/testmirrors.sh
+#test -f testmirrors.sh && chmod +x testmirrors.sh
+#./testmirrors.sh
 # install puppet git
+
+# Ireland
+export MIRROR=https://ftp.heanet.ie/pub/OpenBSD/
+export TMPDIR=/home
+ftp -o create-ami.sh https://raw.githubusercontent.com/ajacoutot/aws-openbsd/master/create-ami.sh                                        
+test -f create-ami.sh && chmod +x create-ami.sh
+create-ami.sh -n
